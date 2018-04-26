1a379eae880d6d4e5f979faad1827a3fd404670a
hxrxchang <m.y.y.0326.cg@gmail.com>
2018-04-26 13:54:06 +0900

    set modal

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,35 @@
 $(() => {
-  console.log('1111111111111111111111111111111');
+  // モーダル表示
+  $('#open-add-memo-modal-btn').on('click', showModal);
+
+  function showModal(event) {
+    event.preventDefault();
+
+    let $shade = $('<div></div>');
+    $shade
+      .attr('id', 'shade')
+      .on('click', hideModal);
+
+    let $modalWin = $('#modalwin');
+    // windowはwindowオブジェクトのこと
+    let $window = $(window);
+    let posX = ($window.width() - $modalWin.outerWidth()) / 2;
+    let posY = ($window.height() - $modalWin.outerHeight()) / 2;
+
+    $modalWin
+      .before($shade)
+      .css({left: posX, top: posY})
+      .removeClass('hide')
+      .addClass('show')
+      .on('click', 'button', () => {
+        hideModal();
+      });
+  }
+
+  function hideModal () {
+    $('#shade').remove();
+    $('#modalwin')
+      .removeClass('show')
+      .addClass('hide');
+  }
 });
